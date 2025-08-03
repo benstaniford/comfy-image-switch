e3fc408cf154bf4c2a54588162a2bb44dfba69e3
Ben Staniford <bstaniford@beyondtrust.com>
2025-08-03 07:08:02 +0100

    Add image switch

--- image_switch_node.py
+++ image_switch_node.py
@@ -1,4 +1,64 @@
 import torch
+from typing import Any, Tuple
+
+class AnyType(str):
+    def __ne__(self, __value: object) -> bool:
+        return False
+
+WILDCARD = AnyType("*")
+
+class SwitchAnyValid:
+    """
+    A switch node that takes up to 4 optional inputs of any type.
+    Selects the first input that is not None.
+    Throws an exception if more than one input is provided (not None).
+    """
+    
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {},
+            "optional": {
+                "input_1": (WILDCARD, {"tooltip": "First optional input"}),
+                "input_2": (WILDCARD, {"tooltip": "Second optional input"}),
+                "input_3": (WILDCARD, {"tooltip": "Third optional input"}),
+                "input_4": (WILDCARD, {"tooltip": "Fourth optional input"}),
+            }
+        }
+    
+    RETURN_TYPES = (WILDCARD,)
+    RETURN_NAMES = ("output",)
+    FUNCTION = "switch_valid"
+    CATEGORY = "utils/switch"
+    
+    def switch_valid(self, input_1: Any = None, input_2: Any = None, input_3: Any = None, input_4: Any = None) -> Tuple[Any]:
+        """
+        Selects the first non-None input. Throws an exception if more than one input is provided.
+        
+        Args:
+            input_1: First optional input
+            input_2: Second optional input
+            input_3: Third optional input
+            input_4: Fourth optional input
+            
+        Returns:
+            Tuple containing the selected input
+            
+        Raises:
+            ValueError: If more than one input is not None
+        """
+        inputs = [input_1, input_2, input_3, input_4]
+        non_none_inputs = [inp for inp in inputs if inp is not None]
+        
+        if len(non_none_inputs) == 0:
+            return (None,)
+        elif len(non_none_inputs) == 1:
+            return (non_none_inputs[0],)
+        else:
+            raise ValueError(f"SwitchAnyValid: Expected exactly one input, but received {len(non_none_inputs)} inputs")
 
 class ImageSwitchNode:
     """
@@ -67,9 +127,11 @@ class ImageSwitchNode:
 
 # Node registration
 NODE_CLASS_MAPPINGS = {
-    "ImageSwitchNode": ImageSwitchNode
+    "ImageSwitchNode": ImageSwitchNode,
+    "SwitchAnyValid": SwitchAnyValid
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "ImageSwitchNode": "Image Switch"
+    "ImageSwitchNode": "Image Switch",
+    "SwitchAnyValid": "Switch Any Valid"
 }
